a9bb279a94ec057aebd280d693eee326bcca745c
JoaoFreire1 <92170119+JoaoFreire1@users.noreply.github.com>
2025-04-03 20:32:05 +0100

    Color class

---
+++ BetterColorSpheres/Color.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BetterColorSpheres
+{
+    public class Color
+    {
+        public byte Red { get;}
+        public byte Green { get;}
+        public byte Blue { get;}
+        public byte Alpha { get;}
+
+        public Color(byte red, byte green, byte blue, byte alpha)
+        {
+            this.Red = red;
+            this.Green = green;
+            this.Blue = blue;
+            this.Alpha = alpha;
+        }
+
+        public byte GetGrey()
+        {
+            return (byte)((Red+Green+Blue)/3);
+        }
+    }
+}
