751aa00f668234653d43b411399d5967a16ccf76
JoaoFreire1 <92170119+JoaoFreire1@users.noreply.github.com>
2025-04-03 20:54:00 +0100

    Highscore value

--- PlayerStats/Player.cs
+++ PlayerStats/Player.cs
@@ -1,14 +1,17 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
 namespace PlayerStats
 {
     public class Player
     {
-        private float highScore;
+        private float highScore = 0;
         private int playedGames;
         private int wonGames;
+
+        private float HighScore {set => (highScore < value) ? (highScore = value) : (highScore = highScore)};
     }
 }
