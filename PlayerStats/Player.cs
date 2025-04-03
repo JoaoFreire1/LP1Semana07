5bc24073c0cdd5ccec024757ee73c67eb11c85a0
JoaoFreire1 <92170119+JoaoFreire1@users.noreply.github.com>
2025-04-03 20:44:37 +0100

    Player class

---
+++ PlayerStats/Player.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlayerStats
+{
+    public class Player
+    {
+        private float highScore;
+        private int playedGames;
+        private int wonGames;
+    }
+}
