a46494ca480479afedf4af37287cca26b3d51245
JoaoFreire1 <92170119+JoaoFreire1@users.noreply.github.com>
2025-04-04 14:12:09 +0100

    XP and Level properties

--- MyRPG/Player.cs
+++ MyRPG/Player.cs
@@ -7,8 +7,13 @@ namespace
 {
     public class Player
     {
-        private int xp;
+        private int xp = 0;
         private float health;
         public string Name {get; set; }
+        public float XP {get => xp; set => xp = (xp + value);}
+        public float Level
+        {
+            get => (level == 0) ? 0 : (float)(1 + xp) / 1000;
+        }
     }
 }
