51872285ee3ace7bf012b211bd82c925f1e3e2b8
JoaoFreire1 <92170119+JoaoFreire1@users.noreply.github.com>
2025-04-04 14:05:59 +0100

    Player Class variables

---
+++ MyRPG/Player.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace 
+{
+    public class Player
+    {
+        private int xp;
+        private float health;
+        public string Name {get; set; }
+    }
+}
